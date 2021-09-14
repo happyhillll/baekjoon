139730670f7096dc2c607834e5c3723efdf3579b
김민지 : ) <happyhill99@gmail.com>
2021-09-14 15:23:25 +0900

    0914

--- 배열/2577 숫자의 개수.py
+++ 배열/2577 숫자의 개수.py
@@ -10,3 +10,9 @@
 출력
 첫째 줄에는 A × B × C의 결과에 0 이 몇 번 쓰였는지 출력한다. 마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다.
 '''
+a=int(input())
+b=int(input())
+c=int(input())
+result = list(str(a*b*c)) #a,b,c 곱한 값을 문자열(str)로 변환 후 list로 묶는다. > result=[1,7,0,3,7,3,0,0]
+for i in range(10):  # i= 0~9 
+    print(result.count(str(i))) #i를 문자열(str)로 바꿔서 result 안에 몇 개 있는지 count
