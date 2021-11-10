d6b64a96886487e048e9e4b18d95c5be37c61218
김민지 : ) <happyhill99@gmail.com>
2021-11-10 23:01:01 +0900

    .

--- 문자열/1316 그룹 단어 체커.py
+++ 문자열/1316 그룹 단어 체커.py
@@ -1,11 +1,12 @@
+# 그룹 단어 찾아서 개수 출력 문제
 N = int(input())
 result = N
 for i in range(0,N):
     word=input()
-    for j in range(0,len(word)-1):
-        if word[j]==word[j+1]:
-            pass
-        elif word[j] in word[j+1:]:
+    for j in range(0,len(word)-1): #인덱스 범위 생성: 0부터 단어개수 -1까지
+        if word[j]==word[j+1]: #연달은 두 문자가 다를때,
+            pass # 현재 글자 이후 문자열을 새로운 단어로 생성
+        elif word[j] in word[j+1:]: 
             result-=1
             break
 print(result)
--- 문자열/2941 크로아티아 알파벳.py
+++ 문자열/2941 크로아티아 알파벳.py
@@ -3,3 +3,45 @@ b = input()
 for i in a:
     b = b.replace(i, 'a')
 print(len(b))
+
+'''
+cro = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+word = input()
+cnt = 0
+for c in cro:
+    if(c in word):
+        cnt += 1
+        wo = word.split(c)
+        word = ''
+        for a in wo:
+            word += a
+cnt += len(word)
+print(cnt)
+word = input()
+cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+cnt = 0
+for i in range(len(cro)):
+    cnt += word.count(cro[i])
+print(len(word)-cnt)
+'''
+
+'''
+croa = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+word = input()
+count = len(word)
+for i in range(0, len(word)-1):
+	for j in croa:
+		if i > 0:
+			if word[i-1] + word[i] + word[i+1] == "dz=":
+				count -= 2
+				i += 1
+				if i >= len(word)-1:
+					break
+				pass
+			if word[i] + word[i+1] == j:
+				count -= 1
+		else:
+			if word[i] + word[i+1] == j:
+				count -= 1
+print(count)	
+'''
